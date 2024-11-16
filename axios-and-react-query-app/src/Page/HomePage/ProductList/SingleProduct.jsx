3f2760095ce29005d9075a080270e978f17eea7d
Sharif-Miah <farhansharif1410@gmail.com>
2024-11-16 22:22:15 +0600

    product delete in axios in react query

--- axios-and-react-query-app/src/Page/HomePage/ProductList/SingleProduct.jsx
+++ axios-and-react-query-app/src/Page/HomePage/ProductList/SingleProduct.jsx
@@ -1,10 +1,32 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+
+const deleteProduct = async (productId) => {
+  const response = await axios.delete(
+    `http://localhost:3000/products/${productId}`
+  );
+  return response.data;
+};
 
 const SingleProduct = ({ product }) => {
   const { id, title, description, price, rating, image } = product;
+  const queryClient = useQueryClient();
+
+  const deleteMutetion = useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+    },
+  });
+
+  const handleDelete = (productId) => {
+    deleteMutetion.mutate(productId);
+    console.log(productId);
+  };
 
   return (
-    <div className='card bg-base-100 w-80 border'>
+    <div className='card bg-base-100 w-80 border '>
       <figure>
         <img
           className='w-56 h-56'
@@ -13,19 +35,21 @@ const SingleProduct = ({ product }) => {
         />
       </figure>
       <div className='card-body'>
-        <h2 className='card-title'>{title.slice(0, 20)}</h2>
-        <p className='text-xl font-semibold'>${price}</p>
-        <p className='text-md font-bold'>Rating: {rating.rate}</p>
-        <p>{description.slice(0, 100)}</p>
-        <div className='flex justify-between gap-4 '>
+        <Link to={`/detailsProduct/${id}`}>
+          <h2 className='card-title'>{title.slice(0, 20)}</h2>
+          <p className='text-xl font-semibold'>${price}</p>
+          <p className='text-md font-bold'>Rating: {rating.rate}</p>
+          <p>{description.slice(0, 90)}</p>
+        </Link>
+        <div className='flex justify-between gap-4 mt-8'>
           <button className='py-2 px-5 text-sm font-bold bg-purple-700 text-white rounded '>
             Buy Now
           </button>
-          <Link to={`/detailsProduct/${id}`}>
-            <button className='py-2 text-sm font-bold px-5 bg-purple-700 text-white rounded '>
-              Show Details
-            </button>
-          </Link>
+          <button
+            onClick={() => handleDelete(product.id)}
+            className='py-2 px-5 text-sm font-bold bg-purple-700 text-white rounded '>
+            Delete
+          </button>
         </div>
       </div>
     </div>
